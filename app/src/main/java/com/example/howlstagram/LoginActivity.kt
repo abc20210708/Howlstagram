d6b8264215dee332958593a1035145e13dd2e665
real <hb743054@gmail.com>
2022-01-03 19:40:21 +0900

    firebase 연동하기

--- app/src/main/java/com/example/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/example/howlstagram/LoginActivity.kt
@@ -1,20 +1,29 @@
 package com.example.howlstagram
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
-    var googleSignInClient : GoogleSignInClient? = null
+
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,39 +39,78 @@ class LoginActivity : AppCompatActivity() {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
+    fun googleLogin() {
+        val signInIntent: Intent = googleSignInClient!!.signInIntent
+        startForResult.launch(signInIntent)
+    }
 
-       fun signinAndSignup() {
-            auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
-                ?.addOnCompleteListener {
-                    task ->
-                        if(task.isSuccessful) {
-                            //아이디가 생성 되었을 때 필요한 코드
-                            moveMainPage(task.result!!.user)
-                        } else if(task.exception?.message.isNullOrEmpty()) {
-                            //로그인 실패 에러 메세지
-                            Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                        } else {
-                            //로그인으로 빠지도록
-                            signinEmail()
-                        }
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+
+            if (result.resultCode == RESULT_OK) {
+                val intent: Intent = result.data!!
+                val task: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(intent)
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d(ContentValues.TAG, "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account) //.idToken!!
+                } catch (e: ApiException) {
+                    Log.w(ContentValues.TAG, "Google sign in filed", e)
                 }
+            }
         }
-    fun signinEmail() {
-        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
-                if(task.isSuccessful) {
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
                     //아이디와 패스워드가 맞을 때
                     moveMainPage(task.result!!.user)
-                }  else {
+                } else {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     }
 
-    fun moveMainPage(user:FirebaseUser?) {
-        if (user != null) {
-            startActivity(Intent(this,MainActivity::class.java))
+        fun signinAndSignup() {
+            auth?.createUserWithEmailAndPassword(
+                email_edittext.text.toString(),
+                password_edittext.text.toString()
+            )
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //아이디가 생성 되었을 때 필요한 코드
+                        moveMainPage(task.result!!.user)
+                    } else if (task.exception?.message.isNullOrEmpty()) {
+                        //로그인 실패 에러 메세지
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    } else {
+                        //로그인으로 빠지도록
+                        signinEmail()
+                    }
+                }
+        }
+
+        fun signinEmail() {
+            auth?.signInWithEmailAndPassword(
+                email_edittext.text.toString(),
+                password_edittext.text.toString()
+            )
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //아이디와 패스워드가 맞을 때
+                        moveMainPage(task.result!!.user)
+                    } else {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+        fun moveMainPage(user: FirebaseUser?) {
+            if (user != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
         }
     }
-}
--- app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.howlstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
-}
+
+
+
+}//
